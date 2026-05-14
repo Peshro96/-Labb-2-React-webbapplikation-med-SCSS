4562d243fc35d4aeb17e97030b88b871fb42022d
Noshin Hussein <peshro.hassan@iths.se>
2026-05-14 20:06:52 +0200

    style home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom";
+
+// Startsidan, hälsar besökaren välkommen och länkar vidare till recepten
 export default function Home() {
   return (
-    <div>
-      <h1>Receptsamling</h1>
-      <p>Välkommen till receptsamlingen.</p>
+    <div className="home">
+      <h1 className="home__title">Receptsamling</h1>
+
+      <p className="home__intro">
+        Hitta dina favoriter, lägg till nya recept och dela med dig av dina
+        bästa rätter.
+      </p>
+
+      <Link to="/recipes" className="home__cta">
+        Bläddra bland recepten
+      </Link>
     </div>
   );
 }
